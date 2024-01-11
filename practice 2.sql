fd503da9b52bcde2bff76a2d4ba8ea2e4649af0b
mfmfvdfv <155982087+mfmfvdfv@users.noreply.github.com>
2024-01-11 22:28:32 +0100

    Update practice 2.sql

--- practice 2.sql
+++ practice 2.sql
@@ -8,3 +8,41 @@ FROM station;
 ------ex3
 select ceil(avg(salary)-avg(replace(salary,0,''))) from EMPLOYEES;
 ------ex4
+
+------ex5
+SELECT candidate_id
+FROM candidates 
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(DISTINCT skill) = 3
+ORDER BY candidate_id ASC;
+------ex6
+SELECT card_name , MAX(issued_amount) - MIN(issued_amount) AS difference 
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
+ -----ex7
+SELECT manufacturer,
+COUNT(drug) AS drug_count, 
+SUM(cogs - total_sales) AS total_loss
+FROM pharmacy_sales
+WHERE cogs > total_sales
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
+----ex8
+SELECT *
+FROM cinema
+WHERE id % 2 <> 0 AND rating DESC;
+---ex9
+SELECT teacher_id, COUNT(DISTINCT subject_id) as cnt
+FROM Teacher
+GROUP BY teacher_id
+---ex10
+select user_id, count(follower_id) as followers_count
+from followers
+group by user_id
+order by user_id
+----ex11
+select class from courses
+group by class
+having count(distinct student)>=5;
