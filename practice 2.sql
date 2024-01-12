340f4218344f145b3154b0158fcc2a49a1cd31ef
mfmfvdfv <155982087+mfmfvdfv@users.noreply.github.com>
2024-01-12 14:10:40 +0100

    Update practice 2.sql

--- practice 2.sql
+++ practice 2.sql
@@ -8,7 +8,9 @@ FROM station;
 ------ex3
 select ceil(avg(salary)-avg(replace(salary,0,''))) from EMPLOYEES;
 ------ex4
-
+select sum(item_count * order_occurrences) as total_items;
+sum(cast(order_occurrences as decimal)) as total orders
+from items_per_order
 ------ex5
 SELECT candidate_id
 FROM candidates 
@@ -16,12 +18,27 @@ WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
 GROUP BY candidate_id
 HAVING COUNT(DISTINCT skill) = 3
 ORDER BY candidate_id ASC;
-------ex6
+-----ex6 
+WITH UserPostCounts AS (
+  SELECT user_id, 
+         COUNT(*) AS post_count, 
+         MIN(post_date) AS first_post_date, 
+         MAX(post_date) AS last_post_date
+    FROM posts
+   WHERE YEAR(post_date) = 07/04/2021 12:00:00
+GROUP BY user_id
+HAVING COUNT(*) >= 2
+)
+
+SELECT user_id, 
+       DATEDIFF(last_post_date, first_post_date) AS days_between
+FROM UserPostCounts;
+------ex7
 SELECT card_name , MAX(issued_amount) - MIN(issued_amount) AS difference 
 FROM monthly_cards_issued
 GROUP BY card_name
 ORDER BY difference DESC;
- -----ex7
+ -----ex8
 SELECT manufacturer,
 COUNT(drug) AS drug_count, 
 SUM(cogs - total_sales) AS total_loss
@@ -29,20 +46,20 @@ FROM pharmacy_sales
 WHERE cogs > total_sales
 GROUP BY manufacturer
 ORDER BY total_loss DESC;
-----ex8
+----ex9
 SELECT *
 FROM cinema
 WHERE id % 2 <> 0 AND rating DESC;
----ex9
+---ex10
 SELECT teacher_id, COUNT(DISTINCT subject_id) as cnt
 FROM Teacher
 GROUP BY teacher_id
----ex10
+---ex11
 select user_id, count(follower_id) as followers_count
 from followers
 group by user_id
 order by user_id
-----ex11
+----ex12
 select class from courses
 group by class
-having count(distinct student)>=5;
+having count(student)>=5;
